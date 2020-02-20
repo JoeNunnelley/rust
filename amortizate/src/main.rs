d0e8e24315466271e9e83f9ff7f403cf8043377f
Joe Nunnelley <joe.nunnelley@tylertech.com>
2020-02-19 16:00:39 -0800

    testing

--- amortizate/src/main.rs
+++ amortizate/src/main.rs
@@ -23,7 +23,9 @@ fn main() {
     io::stdin().read_line(&mut yearly_payments).expect("Failed to read input");
     let _yearly_payments: i32 = yearly_payments.trim().parse().expect("Not a number");
 
-    amortize(_loan_amount, _interest, _loan_term, _yearly_payments);
+    loop {
+        amortize(_loan_amount, _interest, _loan_term, _yearly_payments);
+    }
 }
 
 fn amortize(loan_amount: f64, interest: f64, loan_term: i32, yearly_payments: i32) {
